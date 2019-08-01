2c1ee6294e0a1ec4af9d1be8f9d85571347eadd2
sis02004 <37682403+sis02004@users.noreply.github.com>
2019-08-01 16:43:28 +0900

    Update Server.py

--- Server.py
+++ Server.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import time
 import os
+import threading
 from datetime import datetime
 #socket에서 수신한 버퍼를 반환하는 함수
 def recvall(sock, count):
@@ -14,9 +15,43 @@ def recvall(sock, count):
         buf += newbuf
         count -= len(newbuf)
     return buf
- 
+def receiving():
+    while True:
+    # client에서 받은 stringData의 크기 (==(str(len(stringData))).encode().ljust(16))
+        print("receving")
+        person_name = conn.recv(4)
+        p_name = person_name.decode()
+        length = recvall(conn, 12)
+        stringData = recvall(conn, int(length))
+        data = np.fromstring(stringData, dtype = 'uint8')
+        #data를 디코딩한다.
+        frame = cv2.imdecode(data, cv2.IMREAD_COLOR)
+        cv2.imshow('ImageWindow',frame)
+
+        filename = datetime.now().strftime("SB%Y%m%d_%H%M%S")+"_"+str(i)+".jpg"
+        filedir = datetime.now().strftime("SB_"+str(p_name))
+        print(p_name)
+        print(filename)
+        conn.send(p_name.encode())
+        print("sending")
+        if not os.path.isdir(PATH+filedir):
+            os.mkdir(PATH+filedir)
+
+        #cv2.imwrite(os.path.join(PATH+filedir, filename), frame)
+        cv2.imwrite(PATH+filedir+"/"+filename, frame)
+        cv2.waitKey(1)
+        #data를 디코딩한다.
+
+def sending():
+    print("ready to send")
+    while True:
+        data = p_name
+        data = data.encode()
+        conn.send(data)
+  
 HOST='192.168.0.99'
 PORT=8000
+
 PATH = "C:/Users/SB/Desktop/face/"
 #TCP 사용
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -31,31 +66,10 @@ print('Socket now listening')
  
 #연결, conn에는 소켓 객체, addr은 소켓에 바인드 된 주소
 conn,addr=s.accept()
-i =0
+p_name = "9999"
+  
+threading._start_new_thread(receiving,())
+threading._start_new_thread(sending,())
+
 while True:
-    # client에서 받은 stringData의 크기 (==(str(len(stringData))).encode().ljust(16))
-    print("receving")
-    person_name = conn.recv(4)
-    p_name = person_name.decode()
-    length = recvall(conn, 12)
-    stringData = recvall(conn, int(length))
-    data = np.fromstring(stringData, dtype = 'uint8')
-    #data를 디코딩한다.
-    frame = cv2.imdecode(data, cv2.IMREAD_COLOR)
-    cv2.imshow('ImageWindow',frame)
-
-    filename = datetime.now().strftime("SB%Y%m%d_%H%M%S")+"_"+str(i)+".jpg"
-    filedir = datetime.now().strftime("SB_"+str(p_name))
-    print(p_name)
-    print(filename)
-
-    if not os.path.isdir(PATH+filedir):
-        i = 0
-        os.mkdir(PATH+filedir)
-
-    #cv2.imwrite(os.path.join(PATH+filedir, filename), frame)
-    cv2.imwrite(PATH+filedir+"/"+filename, frame)
-    cv2.waitKey(1)
-    i+=1
-    #data를 디코딩한다.
-    
+    pass
